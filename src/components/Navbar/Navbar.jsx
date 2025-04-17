2f26a574803db333b8ed544e4d7418965dcc22a3
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-18 02:56:32 +0600

    add banner

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,22 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div className="navbar bg-base-100 shadow-sm flex justify-around">
+        <div className="">
+          <h1 className="text-2xl font-bold">
+            <span className="text-blue-400">Auction</span>{" "}
+            <span className="text-yellow-400">Gallery</span>
+          </h1>
+        </div>
+        <nav class="grid grid-flow-col gap-4">
+          <a class="link link-hover">Home</a>
+          <a class="link link-hover">Auctions</a>
+          <a class="link link-hover">Categories</a>
+          <a class="link link-hover">How to works</a>
+        </nav>
+        <div className="flex space-x-4 items-center justify-center">
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -19,61 +31,15 @@ const Navbar = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />{" "}
               </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <div className="w-10 rounded-full">
+          </button>
+          <div className="w-8 rounded-full">
             <img
+              className="rounded-full"
               alt="Tailwind CSS Navbar component"
               src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
             />
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://i.ibb.co.com/cKqFWW9d/Banner-min.jpg)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-2xl font-bold">
+              Bid on Unique Items from <br /> Around the World
+            </h1>
+            <p className="mb-5">
+              Discover rare collectibles, luxury goods, and vintage <br />{" "}
+              treasures in our curated auctions
+            </p>
+            <button className="btn btn-xs px-5 py-3 rounded-full">
+              Explore Auctions
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
