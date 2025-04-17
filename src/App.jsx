f6d0f6fe80d14fd789d4e7f0c425c923aaaa9c9f
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-18 01:31:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </>
+  );
+}
+
+export default App;
