97acdd70ca3abf3692d7a2f032745d68335698e2
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-24 18:20:49 +0600

    resolve

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Blog from "../blog/Blog";
+import { GiSelfLove } from "react-icons/gi";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -10,7 +11,7 @@ const Blogs = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex gap-16 max-w-11/12 mx-auto">
       <div className="overflow-x-auto w-[70%]">
         <table className="table">
           {/* head */}
@@ -30,6 +31,32 @@ const Blogs = () => {
             ))}
           </tbody>
         </table>
+      </div>
+
+      <div className="w-[30%]">
+        <div className="bg-white shadow-md p-4 rounded-md">
+          <div className="flex space-x-3 justify-center">
+            <button>
+              <GiSelfLove size={25} />
+            </button>
+            <h2 className="text-xl font-semibold text-center">
+              Favorite Items
+            </h2>
+          </div>
+
+          <h1 className="text-xl font-semibold mt-7 text-center">
+            No favorites yet
+          </h1>
+          <p className="mt-6 text-center">
+            Click the heart icon on any item <br /> to add it to your favorites
+          </p>
+
+          <div className="flex justify-between mt-7">
+            <h3 className="text-xl font-semibold">Total bids Amount</h3>
+
+            <h2 className="text-xl font-semibold">$0000</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Text = () => {
   return (
     <div>
-      <div className="ml-10 mt-10 mb-7">
+      <div className="ml-16 mt-10 mb-7">
         <h2 className="text-2xl font-bold mb-2">Active Auctions</h2>
         <p>Discover and bid on extraordinary items</p>
       </div>
